8a9b260149d3f44572a16537ded0e82564d7fbbb
anun311 <60570976+anun311@users.noreply.github.com>
2025-03-23 22:41:05 +0700

    Update STL02_LinearRegression.py

--- STL02_LinearRegression.py
+++ STL02_LinearRegression.py
@@ -1,15 +1,31 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
+import io
+
+# URL ของไฟล์ .pkl บน GitHub (แทนที่ด้วย URL ของคุณ)
+model_url = 'https://raw.githubusercontent.com/anun311/py_code/refs/heads/main/mtcar_linear_reg_model.pkl'
+
+def load_model_from_github(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # ตรวจสอบว่าดาวน์โหลดสำเร็จหรือไม่
+        model = pickle.loads(response.content)
+        return model
+    except requests.exceptions.RequestException as e:
+        st.error(f'เกิดข้อผิดพลาดในการดาวน์โหลดโมเดล: {e}')
+        return None
+    except pickle.UnpicklingError as e:
+        st.error(f'เกิดข้อผิดพลาดในการโหลดโมเดลจากไฟล์: {e}')
+        return None
+
+# โหลดโมเดล
+model = load_model_from_github(model_url)
 
 # โหลดโมเดลจากไฟล์ .pkl
-ss = "https://github.com/anun311/py_code/blob/9b596cb84aa702d0f0a333d54b13b4e710af111d/mtcar_linear_reg_model.pkl"
-with open(ss, 'rb') as f:
-    model = pickle.load(f)
-
-print("Coefficients:", model.coef_)
-print("Intercept:", model.intercept_)
-
+# with open('mtcar_linear_reg_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
 inter_ = model.intercept_
 hp_coef = model.coef_[0]
@@ -25,5 +41,11 @@ st.text(f"Intercept: {inter_}")
 st.text(f"hp_coef: {hp_coef}")
 st.text(f"wt_coef: {wt_coef}")
 st.text(f"am_coef: {am_coef}")
+
+st.text(f"var_hp: {var_hp}")
+st.text(f"var_wt: {var_wt}")
+st.text(f"var_am: {var_am}")
+st.text(f"mpg_: {mpg_}")
+
 
 # streamlit run STL02_LinearRegression.py
